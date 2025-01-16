a4386b2d4edac134faec44c4f9923ba32970bdb5
Lyes nefir <nefirlyes@gmail.com>
2025-01-16 14:51:53 +0100

    today's upadate

--- calculateLayer4.cpp
+++ calculateLayer4.cpp
@@ -20,8 +20,11 @@ int i,j,k,m,n;
 
 calculateLayer4_loop: for( i=0;i<100;i++)
 		col_loop: for( j=0;j<50;j++){
+#pragma HLS PIPELINE II=8
 			somme = Layer3_Weights_CPU[i*(1+50*25)];
 			kernelRow_Loop: for( k=0;k<5;k++)
+
+#pragma HLS UNROLL factor=5
 				kernelCol_loop: for ( m=0;m<5;m++)
 					#pragma HLS dependence variable= somme inter RAW distance=150 false
 					somme += Layer3_Weights_CPU[i*(1+50*25)+1 + m + k*5 + j*25] * Layer3_Neurons_CPU[m+5*k+25*j];
