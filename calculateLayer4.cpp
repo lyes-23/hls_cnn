c92bd9ab367dc760100de6f823e0ac337954ea24
Lyes nefir <nefirlyes@gmail.com>
2025-01-27 19:13:26 +0100

    Correct files for layer4

--- calculateLayer4.cpp
+++ calculateLayer4.cpp
@@ -10,10 +10,17 @@ void calculateLayer4(
 		)
 {
 
-#pragma HLS INTERFACE mode=s_axilite port=Layer3_Neurons_CPU
-#pragma HLS INTERFACE mode=s_axilite port=Layer3_Weights_CPU
-#pragma HLS INTERFACE mode=s_axilite port=Layer4_Neurons_CPU
+#pragma HLS INTERFACE mode=m_axi port=Layer3_Neurons_CPU
+#pragma HLS INTERFACE mode=m_axi port=Layer3_Weights_CPU
+#pragma HLS INTERFACE mode=m_axi port=Layer4_Neurons_CPU
 #pragma HLS INTERFACE mode=s_axilite port=return bundle= CTRL_bus
+
+
+
+#pragma HLS BIND_STORAGE variable=Layer3_Weights_CPU type=ram_2p
+#pragma HLS BIND_STORAGE variable=Layer3_Neurons_CPU type=ram_2p
+
+
 
 float somme;
 int i,j,k,m,n;
@@ -21,15 +28,13 @@ int i,j,k,m,n;
 
 calculateLayer4_loop: for( i=0;i<100;i++)
 		col_loop: for( j=0;j<50;j++){
-#pragma HLS PIPELINE II=8
+#pragma HLS PIPELINE II=1
 			somme = Layer3_Weights_CPU[i*(1+50*25)];
 			kernelRow_Loop: for( k=0;k<5;k++)
-
-#pragma HLS UNROLL factor=5
 				kernelCol_loop: for ( m=0;m<5;m++)
-					#pragma HLS dependence variable= somme inter RAW distance=150 false
+					#pragma HLS UNROLL factor=5
 					somme += Layer3_Weights_CPU[i*(1+50*25)+1 + m + k*5 + j*25] * Layer3_Neurons_CPU[m+5*k+25*j];
-		Layer4_Neurons_CPU[i] = (float) SIGMOID(somme);
+		Layer4_Neurons_CPU[i] =  SIGMOID(somme);
 	}
 }
 
