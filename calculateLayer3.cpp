3629fa491d56543bc184d9888f5eacb4b489ca86
FERHOUNE Youba <feryouba0@gmail.com>
2025-01-27 19:05:20 +0100

    adding layer3

--- calculateLayer3.cpp
+++ calculateLayer3.cpp
@@ -1,41 +1,38 @@
 
 #include <math.h>
 #include "calculateLayer3.h"
+#include "sigmoid.h"
 
 void calculateLayer3(
-			float Layer2_Neurons_CPU [6*13*13],
-			float Layer2_Weights_CPU [7800],
-			float Layer3_Neurons_CPU [50*5*5]
-		)
-{
+    float Layer2_Neurons_CPU[6 * 13 * 13],
+    float Layer2_Weights_CPU[7800],
+    float Layer3_Neurons_CPU[50 * 5 * 5]
+) {
 
+    float somme;
+    int i, j, k, m, n;
 
+    calculateLayer3_loop: for (i = 0; i < 50; i++) {
+        row_Loop: for (j = 0; j < 5; j++) {
+            col_loop: for (k = 0; k < 5; k++) {
+				#pragma HLS PIPELINE
+                somme = Layer2_Weights_CPU[26 * 6 * i];
 
+                kernelRow_Loop: for (m = 0; m < 5; m++) {
+                    kernelCol_Loop: for (n = 0; n < 5; n++) {
+                        #pragma HLS UNROLL factor=6
+                        for (int c = 0; c < 6; c++) { // Ajout d'une boucle sur les canaux
+                            somme += Layer2_Weights_CPU[26 * 6 * i + 1 + 6 * (n + 5 * m) + c]
+                                     * Layer2_Neurons_CPU[13 * 13 * c + 13 * (2 * j + m) + (2 * k + n)];
+                        }
+                    }
+                }
 
-float somme;
-int i,j,k,m,n;
-
-
-calculateLayer3_loop: for( i=0;i<50;i++)
-
-	row_Loop: for(j=0;j<5;j++)
-		col_loop: for(k=0;k<5;k++){
-
-			somme = Layer2_Weights_CPU[26*6*i];
-			kernelRow_Loop: for( m=0;m<5;m++)
-				kernelCol_Loop: for( n=0;n<5;n++){
-
-					somme += Layer2_Weights_CPU[26*6*i+1+6*(n+5*m)	] * Layer2_Neurons_CPU[13*13*0+13*(2*j+m)+(2*k+n)];
-					somme += Layer2_Weights_CPU[26*6*i+1+6*(n+5*m)+1] * Layer2_Neurons_CPU[13*13*1+13*(2*j+m)+(2*k+n)];
-					somme += Layer2_Weights_CPU[26*6*i+1+6*(n+5*m)+2] * Layer2_Neurons_CPU[13*13*2+13*(2*j+m)+(2*k+n)];
-					somme += Layer2_Weights_CPU[26*6*i+1+6*(n+5*m)+3] * Layer2_Neurons_CPU[13*13*3+13*(2*j+m)+(2*k+n)];
-					somme += Layer2_Weights_CPU[26*6*i+1+6*(n+5*m)+4] * Layer2_Neurons_CPU[13*13*4+13*(2*j+m)+(2*k+n)];
-					somme += Layer2_Weights_CPU[26*6*i+1+6*(n+5*m)+5] * Layer2_Neurons_CPU[13*13*5+13*(2*j+m)+(2*k+n)];
-						}
-
-					Layer3_Neurons_CPU[5*5*i+5*j+k] = (float) SIGMOID(somme);
-
-					}
+                Layer3_Neurons_CPU[5 * 5 * i + 5 * j + k] = (float) SIGMOID(somme);
+            }
+        }
+    }
 }
+
 
 
--- calculateLayer3.h
+++ calculateLayer3.h
@@ -1,14 +1,18 @@
 #ifndef CALCULATELAYER3_H
 #define CALCULATELAYER3_H
 
-#include "calculateLayer2.h"
+#include <math.h>
+#include <hls_math.h>
+#include <ap_int.h>
+#include <ap_fixed.h>
 
 typedef ap_fixed<16, 5> fixed_p;
+
+#include "sigmoid.h"
 
 #define HW_COSIM
 
 
-#define  SIGMOID(x) (1.7159* hls::tanh(0.66666667*x))
 
 void calculateLayer3(
  		    float Layer2_Neurons_CPU[6*13*13],
@@ -16,5 +20,6 @@ void calculateLayer3(
 			float Layer3_Neurons_CPU [50*5*5]
 
 );
+
 
 #endif
