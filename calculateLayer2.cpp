1e902105051c094413131239ae2dcf1ac1321376
Lyes nefir <nefirlyes@gmail.com>
2025-01-28 15:24:52 +0100

    symetie in sigmoid

--- calculateLayer2.cpp
+++ calculateLayer2.cpp
@@ -1,39 +1,35 @@
 #include "calculateLayer2.h"
 #include <math.h>
 
-
-using namespace std;
-
 void calculateLayer2(
-    ap_uint<8> Layer1_Neurons_CPU[29*29],
-	fixed_p Layer1_Weights_CPU [156],
-	fixed_p Layer2_Neurons_CPU [6*13*13]
+    float Layer1_Neurons_CPU[29*29],
+    float Layer1_Weights_CPU[156],
+    float Layer2_Neurons_CPU[6*13*13]
 ) {
+    #pragma HLS INTERFACE mode=m_axi port=Layer1_Neurons_CPU
+    #pragma HLS INTERFACE mode=bram  port=Layer1_Weights_CPU
+    #pragma HLS INTERFACE mode=m_axi port=Layer2_Neurons_CPU
+    #pragma HLS INTERFACE mode=s_axilite port=return bundle=CTRL_bus
 
 
-#pragma HLS INTERFACE mode=s_axilite port=Layer1_Neurons_CPU
-#pragma HLS INTERFACE mode=s_axilite port=Layer1_Weights_CPU
-#pragma HLS array_partition variable=Layer2_Neurons_CPU complete dim=1
-//#pragma HLS INTERFACE mode=s_axilite port=return bundle= CTRL_bus
 
-	fixed_p somme;
+    float somme;
     int i, j, k, m, n;
-
-#pragma HLS DATAFLOW
 
     calculateLayer2_loop: for (i = 0; i < 6; i++) {
         OutputRow_Loop: for (j = 0; j < 13; j++) {
             OutputColumn_Loop: for (k = 0; k < 13; k++) {
-#pragma HLS PIPELINE II=1
+                #pragma HLS PIPELINE II=25
                 somme = Layer1_Weights_CPU[26 * i];
                 convolutionRow: for (m = 0; m < 5; m++) {
+                    #pragma HLS UNROLL factor=5
                     convolutionColumn: for (n = 0; n < 5; n++) {
-                    somme += Layer1_Weights_CPU[26*i+ 5*m + n + 1 ]
-                    *  Layer1_Neurons_CPU[29*(m+2*j)+ n+ 2*k];
+                        somme += Layer1_Weights_CPU[26*i + 5*m + n + 1] *
+                                 Layer1_Neurons_CPU[29*(m + 2*j) + n + 2*k];
                     }
-#pragma HLS INLINE
-                    Layer2_Neurons_CPU[13*13*i+13*j+k] = (fixed_p) SIGMOID((float)somme);
                 }
+
+                Layer2_Neurons_CPU[13*13*i + 13*j + k] = SIGMOID(somme);
             }
         }
     }
