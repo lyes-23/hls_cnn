5fc1e9fb143442352fa717b9e4cb3812a7c1ed55
Lyes nefir <nefirlyes@gmail.com>
2025-01-30 19:55:07 +0100

    youba's commit

--- cnn_lenet.cpp
+++ cnn_lenet.cpp
@@ -4,8 +4,8 @@
 
 void cnn_lenet(
     float Layer1_Neurons_CPU[29*29],
-	float Layer2_Neurons_CPU[6*13*13],
-	float Layer3_Neurons_CPU[50*5*5],
+	//float Layer2_Neurons_CPU[6*13*13],
+	//float Layer3_Neurons_CPU[50*5*5],
     float Layer1_Weights_CPU[156],
     float Layer2_Weights_CPU[7800],
     float Layer3_Weights_CPU[125100],
@@ -13,16 +13,18 @@ void cnn_lenet(
 )
 {
 
-    #pragma HLS INTERFACE mode=m_axi port=Layer1_Neurons_CPU  offset=slave
-    #pragma HLS INTERFACE mode=bram  port=Layer1_Weights_CPU
-    #pragma HLS INTERFACE mode=bram  port=Layer2_Weights_CPU
-    #pragma HLS INTERFACE mode=bram  port=Layer3_Weights_CPU
-    #pragma HLS INTERFACE mode=m_axi port=Layer4_Neurons_CPU 
-    #pragma HLS INTERFACE mode=s_axilite port=return bundle=CTRL_bus
+#pragma HLS INTERFACE mode=m_axi  port=Layer1_Neurons_CPU depth=841    offset=slave bundle=Layer_Neurons
+#pragma HLS INTERFACE mode=m_axi  port=Layer1_Weights_CPU depth=156    offset=slave bundle=Layer_Weights
+#pragma HLS INTERFACE mode=m_axi  port=Layer2_Weights_CPU depth=7800   offset=slave bundle=Layer_Weights
+#pragma HLS INTERFACE mode=m_axi  port=Layer3_Weights_CPU depth=125100 offset=slave bundle=Layer_Weights
+#pragma HLS INTERFACE mode=m_axi  port=Layer4_Neurons_CPU depth=100    offset=slave bundle=Layer_Neurons
+#pragma HLS INTERFACE mode=s_axilite port=return bundle=CTRL_bus
 
 
 
 
+	float Layer2_Neurons_CPU[6*13*13];
+    float Layer3_Neurons_CPU[50*5*5];
 
     float somme;
     int i, j, k, m, n;
@@ -31,11 +33,11 @@ void cnn_lenet(
     calculateLayer2_loop: for (i = 0; i < 6; i++) {
         OutputRow_Loop: for (j = 0; j < 13; j++) {
             OutputColumn_Loop: for (k = 0; k < 13; k++) {
-                #pragma HLS PIPELINE II=25
+                #pragma HLS PIPELINE II=26
                 somme = Layer1_Weights_CPU[26 * i];
                 convolutionRow: for (m = 0; m < 5; m++) {
-                    #pragma HLS UNROLL factor=5
                     convolutionColumn: for (n = 0; n < 5; n++) {
+#pragma HLS PIPELINE II=2
                         somme += Layer1_Weights_CPU[26*i + 5*m + n + 1] *
                                  Layer1_Neurons_CPU[29*(m + 2*j) + n + 2*k];
                     }
@@ -49,10 +51,11 @@ void cnn_lenet(
     calculateLayer3_loop: for( i = 0; i < 50; i++) {
         row_Loop: for(j = 0; j < 5; j++) {
             col_loop: for(k = 0; k < 5; k++){
-                #pragma HLS PIPELINE II=152
+#pragma HLS PIPELINE II=152
                 somme = Layer2_Weights_CPU[26*6*i];
                 kernelRow_Loop: for(m = 0; m < 5; m++) {
                     kernelCol_Loop: for(n = 0; n < 5; n++){
+#pragma HLS PIPELINE II=2
                         somme += Layer2_Weights_CPU[26*6*i + 1 + 6*(n + 5*m)] * Layer2_Neurons_CPU[13*13*0 + 13*(2*j + m) + (2*k + n)];
                         somme += Layer2_Weights_CPU[26*6*i + 1 + 6*(n + 5*m) + 1] * Layer2_Neurons_CPU[13*13*1 + 13*(2*j + m) + (2*k + n)];
                         somme += Layer2_Weights_CPU[26*6*i + 1 + 6*(n + 5*m) + 2] * Layer2_Neurons_CPU[13*13*2 + 13*(2*j + m) + (2*k + n)];
@@ -71,12 +74,9 @@ calculateLayer4_loop: for( i=0;i<100;i++){
 	somme = Layer3_Weights_CPU[i*(1+50*25)];
 	 col_loop2:for( j=0;j<50;j++)
 		 kernelRow_Loop2: for( k=0;k<5;k++)
-
-#pragma HLS UNROLL factor=5
 			 kernelCol_loop2: for ( m=0;m<5;m++)
-#pragma HLS PIPELINE II=2
+#pragma HLS PIPELINE II=3
 				somme += Layer3_Weights_CPU[i*(1+50*25)+1 + m + k*5 + j*25] * Layer3_Neurons_CPU[m+5*k+25*j];
-
 	Layer4_Neurons_CPU[i] =  SIGMOID(somme);
 }
 
--- cnn_lenet.h
+++ cnn_lenet.h
@@ -7,8 +7,8 @@
 
 void cnn_lenet(
 		    float Layer1_Neurons_CPU[29*29],
-			float Layer2_Neurons_CPU[6*13*13],
-			float Layer3_Neurons_CPU[50*5*5],
+			//float Layer2_Neurons_CPU[6*13*13],
+			//float Layer3_Neurons_CPU[50*5*5],
 		    float Layer1_Weights_CPU[156],
 		    float Layer2_Weights_CPU[7800],
 		    float Layer3_Weights_CPU[125100],
